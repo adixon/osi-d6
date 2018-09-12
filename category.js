fc0df6f0391dc59626a28767246d3b344920ffd2
adixon <alan.g.dixon@gmail.com>
2018-09-12 14:50:35 -0400

    use a class for block hide/show on category pages

--- category.js
+++ category.js
@@ -2,11 +2,11 @@ if (Drupal.jsEnabled) {
   $(document).ready(function () {
     if (true == Drupal.settings.osi.complete) {
       $('#content-content .node-type-category').hide('slow');
-      $('#block-views-Resources-block_4').show('slow');
+      $('.osi-complete').show('slow');
     }
     else {
       $('#content-content .node-type-category').show();
-      $('#block-views-Resources-block_4').hide();
+      $('.osi-complete').hide();
     }
   });
 }
