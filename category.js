544b71a357f07bbe5dcfd1edd05b90cee2dd39e6
root <root@1fdde387c52d>
2019-07-10 15:13:15 +0000

    fix hook_block code and category status block specifically

--- category.js
+++ category.js
@@ -1,12 +1,11 @@
 if (Drupal.jsEnabled) {
   $(document).ready(function () {
     if (true == Drupal.settings.osi.complete) {
-      $('#content-content .node-type-category').hide('slow');
-      $('#block-views-Resources-block_4').show('slow');
+      var resultsLink = '/user/'+Drupal.settings.osi.uid+'/osi_report';
+      $('#content-content .node-type-category').hide('slow').after('<p><strong>Contratulations!</strong></p><p><a href="'+resultsLink+'">View your results</a>');
     }
     else {
       $('#content-content .node-type-category').show();
-      $('#block-views-Resources-block_4').hide();
     }
   });
 }
